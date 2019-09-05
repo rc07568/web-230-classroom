2076d1518cd4ebf5608132c7499a910cb415b203
rcarver <rc07568@ravens.racc.edu>
2019-09-05 15:35:59 -0400

    set up php

---
+++ index.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>web-230-classroom</title>
+</head>
+
+<body>
+	
+	<h1>WEB-230 Classroom</h1>
+	
+	<?php
+		
+		echo '<p>here we go!</p>';	
+		echo 'there we go!';	
+		echo 'where did we go!';
+	?>
+</body>
+</html>
